68fd15f14ccde239e5cbbd6a3b3c1c5738810883
2020093 <2020093@B109-06>
2023-09-08 14:24:49 +0200

    Add project files.

---
+++ Teszt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Késbőbb fejlesztés
+            Console.WriteLine("Én vagyok a teszt 1 program");
+            Console.ReadKey();
+        }
+    }
+}
